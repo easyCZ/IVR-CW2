c822c7321d081bb74a939518ef4082e9c3f60d60
Milan Pavlik <pavlik.mil@gmail.com>
2014-03-24 23:07:43 +0000

    controller bug fix

--- kheperam/controllers/controller/controller.m
+++ kheperam/controllers/controller/controller.m
@@ -1,37 +1,43 @@
 % MATLAB controller for Webots
-% File:          controller.m
-% Date:
-% Description:
-% Author:
-% Modifications:
-
-% uncomment the next two lines if you want to use
-% MATLAB's desktop to interact with the controller:
-%desktop;
-%keyboard;
+% File:          	controller.m
+% Date:				25-03-2013
+% Description:		IVR Coursework 2
+% Author:			Mark Nemec, Milan Pavlik
+% Modifications:	ain't nobody got time for that
 
 TIME_STEP = 64;
+SENSOR_COUNT = 8;
 
-% get and enable devices, e.g.:
-%  camera = wb_robot_get_device('camera');
-%  wb_camera_enable(camera, TIME_STEP);
+% Get and enable distance sensors
+for i = 1 : SENSOR_COUNT
+  ps(i) = wb_robot_get_device(['ds' int2str(i-1)]);
+  wb_distance_sensor_enable(ps(i),TIME_STEP);
+end
 
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
+
+% Main loop
 while wb_robot_step(TIME_STEP) ~= -1
 
-  % read the sensors, e.g.:
-  %  rgb = wb_camera_get_image(camera);
+	% Obtain sensor values
+   	for i=1:N
+        sensor_values(i) = wb_distance_sensor_get_value(ps(i));
+    end
 
-  % Process here sensor data, images, etc.
+  	
+    
+    
 
-  % send actuator commands, e.g.:
-  %  wb_differential_wheels_set_speed(500, -500);
 
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
+	% read the sensors, e.g.:
+	%  rgb = wb_camera_get_image(camera);
+
+	% Process here sensor data, images, etc.
+
+	% send actuator commands, e.g.:
+	%  wb_differential_wheels_set_speed(500, -500);
+
+	% if your code plots some graphics, it needs to flushed like this:
+	drawnow;
 
 end
 
